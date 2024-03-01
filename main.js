861a05a971b945fb5232c2b5849ddf84f4ee6ade
MaggyQuijano <magali.quijano.pliego@gmail.com>
2024-03-01 03:13:17 -0600

    se corrige el portafolio

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let app = document.getElementById('typewriter');
  
 let typewriter = new Typewriter(app, {
   loop: true,
-  delay: 75,
+  delay: 50,
  cursor: "<span style='color: #ad30de;'>|</span>",
 });
  
 typewriter
   .pauseFor(2000)
-  .typeString('<span style="color: ;">Hola! Soy Cristian Magali Quijano Pliego</span>')
+  .typeString('<span style="color: ;">Caminando al éxito</span>')
   .pauseFor(400)
   .deleteChars(20)
   .start();
